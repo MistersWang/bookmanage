61e80862462f70c695a6b7ef433b6e3c997c941e
MistersWang <871158799@qq.com>
2018-05-04 17:35:27 +0800

    总体框架
    总体框架

---
+++ bookmanage.py
@@ -0,0 +1,69 @@
+def Menu():   '''界面'''
+
+def B_addbook():   '''增加书'''
+
+def B_delbook():
+
+def B_modbook():
+
+def B_listbook():
+
+def B_listbrobook():
+
+def M_addmanage():
+
+def M_delmanage():
+
+def M_modmanage():
+
+def M_listmanage():
+
+def S_addstudent()
+
+def S_delstudent():
+
+def S_modstudent():
+
+def S_liststudent():
+
+
+
+# 主函数
+Menu()
+
+B_addbook()
+
+
+B_delbook()
+
+
+B_modbook()
+
+
+B_listbook()
+
+
+B_listbrobook()
+
+
+M_addmanage()
+
+
+M_delmanage()
+
+
+M_modmanage()
+
+
+M_listmanage()
+
+
+S_addstudent()
+
+
+S_delstudent()
+
+S_modstudent()
+
+
+S_liststudent()
